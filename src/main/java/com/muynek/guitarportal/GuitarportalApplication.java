c6ef8ce23517e5e186dc79e4410435ca2b839a4e
muyncio <muynek@gmail.com>
2022-07-14 19:23:34 +0200

    starter

---
+++ src/main/java/com/muynek/guitarportal/GuitarportalApplication.java
@@ -0,0 +1,13 @@
+package com.muynek.guitarportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitarportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitarportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muynek/guitarportal/GuitarportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muynek.guitarportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitarportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
